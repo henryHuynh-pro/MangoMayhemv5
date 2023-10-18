cd63f305b2a41889b56db7dd7d4557e825633550
Senor Orange <joshmoller7@gmail.com>
2023-10-18 09:59:35 -0700

    October 18. 2023 V2

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,6 +4,7 @@ using UnityEngine.UI;
 public class PlayerScript : MonoBehaviour
 {
     public float Jumpforce;
+    public float DoubleJumpforce;
     float score;
 
 
@@ -14,6 +15,15 @@ public class PlayerScript : MonoBehaviour
     Rigidbody2D RB;
 
     public Text ScoreTxt;
+    public float time;
+    public float timeDelay;
+
+
+    void Start()
+    {
+        time = 0f;
+        timeDelay = 0.5f;
+    }
 
     private void Awake()
     {
@@ -23,27 +33,42 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W)) 
+        time = time + 1f * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.W))
         {
             if (isGrounded == true)
             {
                 RB.AddForce(Vector2.up * Jumpforce);
                 isGrounded = false;
-            }
-        }
 
-        if(isAlive)
-        {
-            score += Time.deltaTime * 4;
-            ScoreTxt.text = "SCORE:" + score.ToString("F");
+                if (time >= timeDelay)
+                {
+                    time = 0f;
+
+                    if (isGrounded == false)
+                    {
+                        RB.AddForce(Vector2.up * DoubleJumpforce);
+                        isGrounded = false;
+                    }
+
+
+                }
+            }
+
+            if (isAlive)
+            {
+                score += Time.deltaTime * 4;
+                ScoreTxt.text = "SCORE:" + score.ToString("F");
+            }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.CompareTag("ground"))
+        if (collision.gameObject.CompareTag("ground"))
         {
-            if(isGrounded == false)
+            if (isGrounded == false)
             {
                 isGrounded = true;
             }
@@ -55,7 +80,7 @@ public class PlayerScript : MonoBehaviour
             Time.timeScale = 0;
         }
 
-        if (collision.gameObject.CompareTag("Dexter")) 
+        if (collision.gameObject.CompareTag("Dexter"))
         {
             isAlive = false;
             Time.timeScale = 0;
