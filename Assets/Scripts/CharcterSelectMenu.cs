a59e0874d53882559a2ef68e7c63f8bd1901841f
Senor Orange <joshmoller7@gmail.com>
2023-11-03 13:34:12 -0700

    November 3, 2023 V2

--- Assets/Scripts/CharcterSelectMenu.cs
+++ Assets/Scripts/CharcterSelectMenu.cs
@@ -5,17 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class CharcterSelectMenu : MonoBehaviour
 {
+    //Game
     public string MangoMayhem;
 
+    //Players For Run Game
     public GameObject MiguelPlayer;
+    public GameObject HenryPlayer;
+    public GameObject BrandonPlayer;
 
+    //Miguel Loader
     public void PlayMiguel()
     {
-      StartCoroutine(LoadYourAsyncScene());
-        
+        StartCoroutine(LoadSceneWithMiguel());
+
     }
 
-    IEnumerator LoadYourAsyncScene()
+    IEnumerator LoadSceneWithMiguel()
     {
         // Set the current Scene to be able to unload it later
         Scene currentScene = SceneManager.GetActiveScene();
@@ -37,5 +42,68 @@ public class CharcterSelectMenu : MonoBehaviour
 
         // Instantiates the Prefab as a GameObject
         GameObject newInstance = Instantiate(MiguelPlayer);
+    }
+
+    //Henry Loader
+    public void PlayHenry()
+    {
+        StartCoroutine(LoadSceneWithHenry());
+
+    }
+
+    IEnumerator LoadSceneWithHenry()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(MangoMayhem, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(HenryPlayer, SceneManager.GetSceneByName(MangoMayhem));
+
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+
+        // Instantiates the Prefab as a GameObject
+        GameObject newInstance = Instantiate(HenryPlayer);
+    }
+
+
+    //Brandon Loader
+    public void PlayBrandon()
+    {
+        StartCoroutine(LoadSceneWithBrandon());
+
+    }
+
+    IEnumerator LoadSceneWithBrandon()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(MangoMayhem, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(BrandonPlayer, SceneManager.GetSceneByName(MangoMayhem));
+
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+
+        // Instantiates the Prefab as a GameObject
+        GameObject newInstance = Instantiate(BrandonPlayer);
     }
 }
