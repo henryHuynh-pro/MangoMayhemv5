fee3b5eee2ed9accb8a4cc32bd65d975df9090a0
Senor Orange <joshmoller7@gmail.com>
2023-11-28 15:08:05 -0800

    November 28, 2023 V2

--- Assets/Scripts/CharcterSelectMenu.cs
+++ Assets/Scripts/CharcterSelectMenu.cs
@@ -16,6 +16,7 @@ public class CharcterSelectMenu : MonoBehaviour
     public GameObject BrandonPlayer;
     public GameObject HansenPlayer;
     public GameObject NguyenPlayer;
+    public GameObject JaxsonPlayer;
 
 
     //Miguel Loader
@@ -180,6 +181,38 @@ public class CharcterSelectMenu : MonoBehaviour
 
         // Instantiates the Prefab as a GameObject
         Instantiate(NguyenPlayer);
+        Instantiate(Score);
+    }
+
+    public void PlayJaxson()
+    {
+        StartCoroutine(LoadSceneWithJaxson());
+
+    }
+
+    IEnumerator LoadSceneWithJaxson()
+    {
+        // Set the current Scene to be able to unload it later
+        Scene currentScene = SceneManager.GetActiveScene();
+
+        // The Application loads the Scene in the background at the same time as the current Scene.
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(MangoMayhem, LoadSceneMode.Additive);
+
+        // Wait until the last operation fully loads to return anything
+        while (!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
+        SceneManager.MoveGameObjectToScene(JaxsonPlayer, SceneManager.GetSceneByName(MangoMayhem));
+        SceneManager.MoveGameObjectToScene(Score, SceneManager.GetSceneByName(MangoMayhem));
+
+        // Unload the previous Scene
+        SceneManager.UnloadSceneAsync(currentScene);
+
+        // Instantiates the Prefab as a GameObject
+        Instantiate(JaxsonPlayer);
         Instantiate(Score);
     }
 }
