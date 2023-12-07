35d993fbd4c4e179ea54813f4da5d2e568cf55b7
Senor Orange <joshmoller7@gmail.com>
2023-12-07 12:42:35 -0800

    Unity Version Upgrade

--- Assets/Scripts/RunButtons.cs
+++ Assets/Scripts/RunButtons.cs
@@ -10,26 +10,28 @@ public class RunButtons : MonoBehaviour
     public GameObject ScoreCounter;
     public void Quit()
     {
+        //ScoreCounter = GameObject.FindGameObjectWithTag("CoinCounter");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 
-        StartCoroutine(MoveScoreToMenu());
-        
-        
-        Destroy(GameObject.FindWithTag("Player"));
-        
+        //StartCoroutine(MoveScoreToMenu());
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("CoinCounter");
+        foreach (GameObject go in gos)
+            Destroy(go);
+
+
     }
 
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 
-    }
+    }   
 
     IEnumerator MoveScoreToMenu()
     {
         // Set the current Scene to be able to unload it later
         Scene currentScene = SceneManager.GetActiveScene();
-        ScoreCounter = GameObject.FindGameObjectWithTag("CoinCounter");
+        //ScoreCounter = GameObject.FindGameObjectWithTag("CoinCounter");
 
         // The Application loads the Scene in the background at the same time as the current Scene.
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
@@ -41,14 +43,14 @@ public class RunButtons : MonoBehaviour
         }
 
         // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
-        SceneManager.MoveGameObjectToScene(ScoreCounter, SceneManager.GetSceneByName(Menu));
-        
+        //SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneByName(Menu));
+        //SceneManager.MoveGameObjectsToScene()
 
         // Unload the previous Scene
         SceneManager.UnloadSceneAsync(currentScene);
 
         // Instantiates the Prefab as a GameObject
         
-        //Instantiate(CoinCounter);
+        Instantiate(ScoreCounter);
     }
 }
