acbd599a32ed66d4e74d5c1f7818e9e22355c45d
Senor Orange <joshmoller7@gmail.com>
2023-12-07 13:48:02 -0800

    December 7, 2023 V1

--- Assets/Scripts/RunButtons.cs
+++ Assets/Scripts/RunButtons.cs
@@ -7,50 +7,48 @@ public class RunButtons : MonoBehaviour
 {
     public string Menu;
 
-    public GameObject ScoreCounter;
+    public GameObject Coins;
+
+        
     public void Quit()
     {
         //ScoreCounter = GameObject.FindGameObjectWithTag("CoinCounter");
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 
-        //StartCoroutine(MoveScoreToMenu());
-        GameObject[] gos = GameObject.FindGameObjectsWithTag("CoinCounter");
-        foreach (GameObject go in gos)
-            Destroy(go);
+        StartCoroutine(MoveScoreToMenu());
 
-
+        
     }
 
     public void Retry()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
 
-    }   
+
+    }
+
 
     IEnumerator MoveScoreToMenu()
     {
-        // Set the current Scene to be able to unload it later
         Scene currentScene = SceneManager.GetActiveScene();
-        //ScoreCounter = GameObject.FindGameObjectWithTag("CoinCounter");
 
-        // The Application loads the Scene in the background at the same time as the current Scene.
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
 
-        // Wait until the last operation fully loads to return anything
+
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(Menu, LoadSceneMode.Additive);
+
         while (!asyncLoad.isDone)
         {
             yield return null;
         }
 
-        // Move the GameObject (you attach this in the Inspector) to the newly loaded Scene
-        //SceneManager.MoveGameObjectToScene(go, SceneManager.GetSceneByName(Menu));
-        //SceneManager.MoveGameObjectsToScene()
 
-        // Unload the previous Scene
+
+        SceneManager.MoveGameObjectToScene(Coins, SceneManager.GetSceneByName(Menu));
+
+        
         SceneManager.UnloadSceneAsync(currentScene);
 
-        // Instantiates the Prefab as a GameObject
+       
         
-        Instantiate(ScoreCounter);
     }
 }
